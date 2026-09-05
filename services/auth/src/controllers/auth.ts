5d681b96c7db5e09ce9e1724bf073b545f848fec
200-hassan <md.arhassan@gmail.com>
2026-09-05 23:48:16 +0600

    setup utils

--- services/auth/src/controllers/auth.ts
+++ services/auth/src/controllers/auth.ts
@@ -1,3 +1,5 @@
+import axios from "axios";
+import getBuffer from "../utils/buffer.js";
 import { sql } from "../utils/db.js";
 import ErrorHandler from "../utils/errorHandler.js";
 import { TryCatch } from "../utils/TryCatch.js";
@@ -32,10 +34,24 @@ export const  registerUser = TryCatch(async(req,res,next)=>{
 
         const file = req.file;
 
+        if(!file){
+            throw new ErrorHandler(400, "Resume file is required for jobseekers");
+        }
+
+        const fileBuffer = getBuffer(file);
+
+        if(!fileBuffer || !fileBuffer.content){
+            throw new ErrorHandler(500, "failed to generate buffer");
+        }
+
+        const {data} = await axios.post(`${process.env.UPLOAD_SERVICE}/api/utils/upload`,
+            {buffer: fileBuffer.content}
+        );
+
         const [user] = 
-            await sql`INSERT INTO users (name, email, password, phone_number, role) VALUES
-                      (${name}, ${email}, ${hashPassword}, ${phoneNumber}, ${role}) RETURNING
-                      user_id, name, email, phone_number, role, created_at`;
+            await sql`INSERT INTO users (name, email, password, phone_number, role, bio, resume, resume_public_id) VALUES
+                      (${name}, ${email}, ${hashPassword}, ${phoneNumber}, ${role}, ${bio}, ${data.url}, ${data.public_id}) RETURNING
+                      user_id, name, email, phone_number, role, bio, resume, created_at`;
     }
 
     res.json(email);
