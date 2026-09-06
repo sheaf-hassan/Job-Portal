6c28aa68f15d3c6bba26932c4402307580374d72
200-hassan <md.arhassan@gmail.com>
2026-09-06 23:13:13 +0600

    create resetPassword

--- services/auth/src/controllers/auth.ts
+++ services/auth/src/controllers/auth.ts
@@ -164,3 +164,51 @@ export const forgotPassword = TryCatch(async(req, res, next)=>{
         message: "If that email exists, we will sent a reset link",
     });
 });
+
+export const resetPassword = TryCatch(async (req, res, next) => {
+    const { token } = req.params;
+    const { password } = req.body;
+
+    if (Array.isArray(token)) {
+        throw new ErrorHandler(400, "Invalid token");
+    }
+
+    let decoded: any;
+
+    try {
+        decoded = jwt.verify(
+            token,
+            process.env.JWT_SEC as string
+        );
+    } catch (error) {
+        throw new ErrorHandler(400, "Expired token");
+    }
+
+    if(decoded.type !== "reset"){
+        throw new ErrorHandler(400, "Invalid token type");
+    }
+
+    const email = decoded.email;
+
+    const storedToken = await redisClint.get(`forgot:${email}`);
+
+    if(!storedToken || storedToken !== token){
+        throw new ErrorHandler(400, "Token has been expired");
+    }
+
+    const users = await sql`SELECT user_id FROM users WHERE email = ${email}`;
+    
+    if(users.length === 0){
+        throw new ErrorHandler(404, "User not found");
+    }
+
+    const user = users[0];
+
+    const hashPassword = await bcrypt.hash(password, 10);
+
+    await sql`UPDATE users SET password = ${hashPassword} WHERE user_id = ${user.user_id}`;
+
+    await redisClint.del(`forgot:${email}`);
+
+    res.json({message: "Password changed successfully"});
+});
