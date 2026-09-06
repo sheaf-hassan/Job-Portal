d2d8d0bd6622d5ba80fe7628de186f3f2453bfed
200-hassan <md.arhassan@gmail.com>
2026-09-06 19:36:16 +0600

    create producer

---
+++ services/auth/src/producer.ts
@@ -0,0 +1,46 @@
+import { Kafka, Producer, Admin } from "kafkajs";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+let producer: Producer;
+let admin: Admin;
+
+export const connectKafka = async () => {
+  try {
+    const kafka = new Kafka({
+      clientId: "auth-service",
+      brokers: [process.env.kafka_Broker || "localhost:9092"],
+    });
+
+    admin = kafka.admin();
+    await admin.connect();
+
+    const topics = await admin.listTopics();
+
+    if (!topics.includes("send-mail")) {
+      await admin.createTopics({
+        topics: [
+          {
+            topic: "send-mail",
+            numPartitions: 1,
+            replicationFactor: 1,
+          },
+        ],
+      });
+
+      console.log("✅ Topic 'send-mail' created");
+    }
+
+    await admin.disconnect();
+
+    producer = kafka.producer();
+
+    await producer.connect();
+
+    console.log("✅ Connected to kafka producer");
+    
+  } catch (error) {
+    console.log("Failed to connect to kafka", error);
+  }
+};
