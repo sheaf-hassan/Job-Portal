df19ec4756a0ee6bffa00341d791aa6fbfc91376
200-hassan <md.arhassan@gmail.com>
2026-09-06 19:47:27 +0600

    create publishToTopic

--- services/auth/src/producer.ts
+++ services/auth/src/producer.ts
@@ -39,8 +39,35 @@ export const connectKafka = async () => {
     await producer.connect();
 
     console.log("✅ Connected to kafka producer");
-    
+
   } catch (error) {
     console.log("Failed to connect to kafka", error);
   }
+};
+
+export const publishToTopic = async(topic:string, message:any) => {
+    if(!producer) {
+        console.log("kafka producer is not initialized");
+        return;
+    }
+
+    try {
+        await producer.send({
+            topic: topic,
+            messages: [
+                {
+                    value: JSON.stringify(message),
+                },
+            ],
+        });
+
+    } catch (error) {
+        console.log("failed to publish message to kafka", error);
+    }
+};
+
+export const disconnectKafka = async() => {
+    if (producer) {
+        producer.disconnect();
+    }
 };
