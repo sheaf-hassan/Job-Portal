6d22d9c584c84903665d6eeb76cefb01abc87510
200-hassan <md.arhassan@gmail.com>
2026-09-07 20:09:30 +0600

    create updateProfilePic

--- services/user/src/routes/user.ts
+++ services/user/src/routes/user.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { isAuth } from "../middleware/auth.js";
-import { getUserProfile, myProfile, updateUserProfile } from "../controllers/user.js";
+import { getUserProfile, myProfile, updateProfilePic, updateUserProfile } from "../controllers/user.js";
+import uploadFile from "../middleware/multer.js";
 
 
 const router= express.Router();
@@ -8,5 +9,6 @@ const router= express.Router();
 router.get("/me", isAuth, myProfile);
 router.get("/:userId", isAuth, getUserProfile);
 router.put("/update/profile", isAuth, updateUserProfile);
+router.put("/update/pic", isAuth, uploadFile, updateProfilePic);
 
 export default router;
